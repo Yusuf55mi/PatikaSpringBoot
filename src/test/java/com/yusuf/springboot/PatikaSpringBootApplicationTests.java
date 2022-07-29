088600981ba182a4b50edb89b7cafeccacef047a
Yusuf AYDIN <yusufaydin@Yusuf-MacBook-Air.local>
2022-07-29 17:34:39 +0300

    First Commit

---
+++ src/test/java/com/yusuf/springboot/PatikaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusuf.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatikaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
